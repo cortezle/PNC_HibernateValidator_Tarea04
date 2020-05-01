3390508f6d4a2e8a9972094b0214901b5ce48ee9
cortezle <00029117@uca.edu.sv>
2020-05-01 15:28:29 -0600

    first commit

---
+++ src/test/java/com/uca/capas/hibernateValidator/HibernateValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.hibernateValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
